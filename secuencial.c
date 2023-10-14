c7d707004fa5545c31325f388364fcc320a82de7
Fredy Velásquez <59968764+fredyvelasquezgt@users.noreply.github.com>
2023-10-14 17:17:41 -0600

    Create secuencial.c

---
+++ secuencial.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+// Función para calcular la suma de Riemann en un subintervalo
+double calcular_suma_riemann(double a, double b, int num_subintervalos) {
+    double delta_x = (b - a) / num_subintervalos;
+    double suma_parcial = 0.0;
+    for (int i = 0; i < num_subintervalos; i++) {
+        double x_i = a + i * delta_x;
+        double f_x_i = x_i * x_i;  // Reemplaza con tu función
+        suma_parcial += f_x_i * delta_x;
+    }
+    return suma_parcial;
+}
+
+int main() {
+    double a = 0.0;
+    double b = 1.0;
+    int num_subintervalos = 1000000; // Ajustar según sea necesario
+
+    double start_time, end_time;
+    
+    // Versión secuencial
+    start_time = omp_get_wtime();
+    double suma_secuencial = calcular_suma_riemann(a, b, num_subintervalos);
+    end_time = omp_get_wtime();
+    printf("Aproximación de la integral (secuencial): %f\n", suma_secuencial);
+    printf("Tiempo de ejecución (secuencial): %f segundos\n", end_time - start_time);
+
+    return 0;
+}
